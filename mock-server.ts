1a286c974f380ec2afb12adb3779c63acee80506
corn12138 <15330485163@163.com>
2024-11-20 10:58:13 +0800

    mock数据

--- mock-server.ts
+++ mock-server.ts
@@ -1,11 +1,11 @@
-import {createMiddleware} from "@mswjs/http-middleware"
+import { createMiddleware } from "@mswjs/http-middleware"
 import cors from "cors"
 import express from "express"
 import logger from 'pino-http' // 这个是一个日志库
 
-import {env} from "./src/config/env";
-import {initializeDb}from './src/testing/mocks/db';
-import {handles} from './src/testing/mocks/handles';
+import { env } from "./src/config/env";
+import { initializeDb } from './src/testing/mocks/db';
+import { handles } from './src/testing/mocks/handles';
 
 const app = express(); // 创建 express 应用
 
@@ -19,9 +19,9 @@ app.use(express.json());
 app.use(logger());
 app.use(createMiddleware(...handles)); // 使用 MSW 的中间件
 
-initializeDb().then(()=>{
+initializeDb().then(() => {
     console.log("Mock server is running");
-    app.listen(env.APP_MOCK_API_PORT,()=>{
+    app.listen(env.APP_MOCK_API_PORT, () => {
         console.log(`Mock server is running on http://localhost:${env.APP_MOCK_API_PORT}`);
     })
 }); // 初始化数据库
