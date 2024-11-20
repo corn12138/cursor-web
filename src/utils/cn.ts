799496153681a180b559004cf22be375acdc4839
corn12138 <15330485163@163.com>
2024-11-20 11:11:31 +0800

    changes to be committed: modified: src/utils/cn.ts

--- src/utils/cn.ts
+++ src/utils/cn.ts
@@ -1,5 +1,4 @@
 import { type ClassValue, clsx } from "clsx"; // clsx的作用是合并类名
-
 import { twMerge } from "tailwind-merge"; //此插件的作用是合并Tailwind CSS的类名
 export const cn = (...inputs: ClassValue[]) => {
     return twMerge(clsx(...inputs))
